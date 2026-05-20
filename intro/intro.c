21dedc4e8af0171014e4ff05d038dec2252f085a
John Owuor <owuor91@gmail.com>
2026-05-20 17:15:36 +0300

    add cpu notes

--- intro/intro.c
+++ intro/intro.c
@@ -6,7 +6,10 @@
 
 
 // argc - Argument count: Number of arguments passed to the program including program name
-//argv - Argument vector: Array containing proram name and all arguments passed
+// argv - Argument vector: Array containing proram name and all arguments passed
+// checks if exactly 2 arguments have been passed then assigns arg string argv[1] to *str
+// before looping infinitely to check the time every second and print str
+// running multiple instance with ./cpu A & ./cpu B & ./cpu C & ... demonstrates cpu virtualization 
 int main(int argc, char *argv[]){
     if(argc != 2){
         fprintf(stderr, "usage: cpu <string>\n");
