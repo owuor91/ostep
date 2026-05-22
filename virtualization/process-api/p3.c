2db62c91165156e3a84f36e18bb5a4aeffc6b37c
John Owuor <owuor91@gmail.com>
2026-05-22 18:37:49 +0300

    document example of fork(), wait(), exec() in shell

--- virtualization/process-api/p3.c
+++ virtualization/process-api/p3.c
@@ -34,4 +34,11 @@ on the same file then invokes wc using execvp(). execvp() overrides the current 
 the heap and stack of the program, effectively transforming the current program into another one (wc).
 Since a successful call to execvp never returns line 23 doesn't execute. 
 exec is used to run a different program within the current process as opposed to fork() which runs a copy of the same program
+
+The application of fork(), exec() and wait() is for example in the shell when a user types a command (name of an executable program + arguments):
+1. shell figures out where to find the executable in the file system, 
+2. calls fork() to start a child process within which to run the command
+3. calls exec() to run the command,(a different program)
+4. calls wait() to wait for the command to complete
+5. when the command completes, shell returns from wait() and prints out a prompt waiting for the next command
 */
